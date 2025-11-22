4f2fda9ec5088f9f901f56096593d18282c45415
DataPurity <info@datapurity.com>
2025-11-22 05:14:39 +0300

    fix: Reverse logo arrow direction for RTL (Arabic) interface
    Changed droplet arrow from left-pointing to right-pointing to match RTL layout

--- frontend/src/layouts/AppShell/TopBar.tsx
+++ frontend/src/layouts/AppShell/TopBar.tsx
@@ -29,7 +29,7 @@ function TopBar() {
         <div className="topbar-left">
           <div className="topbar-logo">
             <svg width="28" height="28" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M50 10C50 10 25 35 25 55C25 70 35 80 50 80C65 80 75 70 75 55C75 35 50 10 50 10Z" fill="url(#gradient-topbar)"/>
+              <path d="M50 10C50 10 75 35 75 55C75 70 65 80 50 80C35 80 25 70 25 55C25 35 50 10 50 10Z" fill="url(#gradient-topbar)"/>
               <rect x="35" y="45" width="30" height="4" rx="2" fill="white" opacity="0.9"/>
               <rect x="35" y="53" width="30" height="4" rx="2" fill="white" opacity="0.9"/>
               <rect x="35" y="61" width="30" height="4" rx="2" fill="white" opacity="0.9"/>
