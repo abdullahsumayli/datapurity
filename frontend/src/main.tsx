1f16317f2b8a6ee57944552544b5fa4ae9c3da9f
DataPurity <info@datapurity.com>
2025-11-29 19:22:42 +0300

    Remove Web3 guard - allow browser extensions

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { disableWeb3 } from './utils/web3Guard'
 // import { registerServiceWorker, showInstallPrompt } from './pwa/registerSW'
 // تعليق تسجيل service worker مؤقتاً لمنع أخطاء الكونسول
 // registerServiceWorker({
@@ -11,9 +10,6 @@ import { disableWeb3 } from './utils/web3Guard'
 // Show install prompt when available
 // showInstallPrompt()
 import './styles/globals.css'
-
-// Disable any injected Web3 providers (e.g., MetaMask)
-disableWeb3()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
